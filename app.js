2699d6a5090d66d6d880c07745ff51d1749232ce
Eric McGary <emcgary@resourceammirati.com>
2016-07-05 15:31:18 -0400

    Adding other esentials

--- app.js
+++ app.js
@@ -35,6 +35,10 @@ app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+app.use(express.static(path.join(__dirname, '../node_modules')));
+app.use(express.static(path.join(__dirname,'public')));
+
 app.use(compression());
 
 // route handling
@@ -63,9 +67,4 @@ app.use((err, req, res) => {
 // === Startup ================================================
 // ============================================================
 
-var httpServer = http.createServer(app);
-const PORT = process.env.PORT || 3000;
-
-httpServer.listen(PORT, () => {
-    console.log('HTTP server running at %d', PORT);
-});
+module.exports = app;
